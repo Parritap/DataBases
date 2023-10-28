56329ba7366155e30504cafb8565cf96e230315d
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2023-10-28 13:20:59 -0500

    MAX(column) clause

--- groupBy.sql
+++ groupBy.sql
@@ -1,23 +1,21 @@
 USE store_db;
 
 /**
-GROUṔ BY : is used to get set SET (so no repetition is admited) of the elements specified. For example
-Lets imagine we have a table of customers with a column "country". Lets say that 5 of them are from Mexico, so 
-we have 5 records where customer is from Mexico.
-
-Now, the next clause will list the name of countries in the table Customers 
-
-SELECT  Country
-FROM Customers
-GROUP BY Country;
-
-Here, Mexico will only appear once along with all the countries that appear in the table.
-
-**/
-
-
-
--- Filter the name and id of customers that have bougth something at least onces.
+ GROUP BY : is used to get set SET (so no repetition is admited) of the elements specified. For example
+ Lets imagine we have a table of customers with a column "country". Lets say that 5 of them are from Mexico, so 
+ we have 5 records where customer is from Mexico.
+ 
+ Now, the next clause will list the name of countries in the table Customers 
+ 
+ SELECT  Country
+ FROM Customers
+ GROUP BY Country;
+ 
+ Here, Mexico will only appear once along with all the countries that appear in the table.
+ 
+ **/
+-- FILTER THE NAME AND ID OF CUSTOMERS THAT HAVE BOUGTH SOMETHING AT LEAST ONCE.
+-- FILTRAR EL NOMBRE Y EL ID DE LOS CLIENTES QUE HAN COMPRADO AL MENOS UNA VEZ EN LA TIENDA.
 /*
  Note that when  using alias, both relations (customer and the join table) 
  most have, each, said alias, i.e, there are two "AS" usages.  
@@ -33,17 +31,39 @@ FROM
         FROM
             orders
         GROUP BY
-            customer_id 
+            customer_id
     ) as subquery ON c.customer_id = subquery.customer_id;
 
 
-
-
 -- How to count how many times does one column value repeats in a table? 
--- COUNTY BY can be used along with other funtions like COUNT. 
+-- NOTE: COUNTY BY can be used along with other funtions like COUNT. 
+-- LISTAR TODOS LOS PAÍSES DE LOS QUE HACEN PARTE LOS CLIENTES. 
+SELECT
+    Country
+FROM
+    customers
+GROUP BY
+    Country;
 
-SELECT  Country
-FROM Customers
-GROUP BY Country;
 
-        
+
+-- USING GROUP BY WITH AGGREGATE FUNCTIONS: 
+-- MAX(COLUMN)
+-- IMPRIMIR LAS COMPRAS CON EL VALOR MÁS ALTO DE CADA CLIENTE, JUNTO CON SU NOMBRE COMPLETO E ID. 
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    s.max
+FROM
+    customers as c
+    JOIN (
+        SELECT
+            customer_id,
+            MAX(total_amount) as max
+        FROM
+            orders
+        GROUP BY
+            customer_id
+    ) AS s ON s.customer_id = c.customer_id;
