11cbe607d583b27d72c4d6ec642b014acf30c078
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2024-05-12 16:35:52 -0500

    Avance poblacion proyecto

--- Oracle/Modelos/store/parcial.sql
+++ Oracle/Modelos/store/parcial.sql
@@ -74,24 +74,24 @@ where CATEGORIA_IDCATEGORIA = '1'
 connect by prior p.IDPRODUCTO = p.ID_PRODUCTO_COMPUESTO
 
 
--- ------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- PUNTO 2 --> CORRECTO.
-select sub.IDPRODUCTO id,
+select sub.IDPRODUCTO             id,
        sub.NOMBREPRODUCTO,
        sub.lvl,
-       SUM (GANANCIA * d.CANTIDAD) ganancias
+       SUM(GANANCIA * d.CANTIDAD) ganancias
 from (select p.IDPRODUCTO,
              p.NOMBREPRODUCTO,
-             level as lvl,
-             (P.PRECIOVENTA - P.PRECIOCOMPRA ) ganancia
+             level as                         lvl,
+             (P.PRECIOVENTA - P.PRECIOCOMPRA) ganancia
       from PRODUCTO p
       connect by prior p.IDPRODUCTO = p.ID_PRODUCTO_COMPUESTO
       start with (p.CATEGORIA_IDCATEGORIA = '1')) sub
-left join DETALLEVENTA d on (sub.IDPRODUCTO = d.IDPRODUCTO)
+         left join DETALLEVENTA d on (sub.IDPRODUCTO = d.IDPRODUCTO)
 group by sub.IDPRODUCTO, sub.NOMBREPRODUCTO, sub.lvl;
 
 
--------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Punto 3 --->
 
 /* Genere una consulta en la cual por cada cliente se muestre su nombre, si su dirección
@@ -112,3 +112,77 @@ select  c.IDCLIENTE id,
 )) sub on sub.IDCLIENTE = c.IDCLIENTE
 left join DIRECCION d1 on (c.IDCLIENTE = d1.IDCLIENTE and d1.IDTIPO = (select IDTIPO from TIPODIRECCION where DESCRIPCION=' Tipo 1 - Trabajo'))
 left join DIRECCION d2 on (c.IDCLIENTE = d2.IDCLIENTE and d2.IDTIPO = (select IDTIPO from TIPODIRECCION where DESCRIPCION=' Tipo 2 - Dimicilio'))
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    SELECT
+    c.NOMBRE AS nombre_cliente,
+    COALESCE(dt.DIRECCIÓN, dw.DIRECCIÓN, 'Ninguna') AS direccion,
+    CASE
+        WHEN EXISTS (SELECT 1 FROM FACTURAVENTA f WHERE f.IDCLIENTE = c.IDCLIENTE AND f.IDESTADO = 'Cancelada') THEN 'No Confiable'
+        ELSE 'Confiable'
+    END AS confiable
+FROM
+    CLIENTE c
+    LEFT JOIN DIRECCION dt ON c.IDCLIENTE = dt.IDCLIENTE AND dt.IDTIPO = (SELECT IDTIPO FROM TIPODIRECCION WHERE DESCRIPCION = 'Tipo 2 - Domicilio')
+    LEFT JOIN DIRECCION dw ON c.IDCLIENTE = dw.IDCLIENTE AND dw.IDTIPO = (SELECT IDTIPO FROM TIPODIRECCION WHERE DESCRIPCION = 'Tipo 1 - Trabajo')
+    LEFT JOIN FACTURAVENTA f on c.IDCLIENTE = f.IDCLIENTE.
+WHERE
+    EXISTS (SELECT 1 FROM FACTURAVENTA f WHERE f.IDCLIENTE = c.IDCLIENTE);
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Punto 4.
+
+/*
+Pensando en optimización, Cree una vista que permita conocer:
+por color,
+por nombre de producto y categoría,
+por categoría y color,
+por tipo; el total de unidades vendidas y la cantidad de clientes que han comprado,
+debe mostrar siempre el id y descripción de cada uno de los conceptos solicitados.
+
+Realice luego una consulta que permita conocer el total de clientes y cantidad de unidades vendidas por categoría, especificando el color.
+
+select
+id_producto
+nombre_producto
+idcolor,
+color,
+id_categoria
+categoria
+
+*/
+
+
+
+select p.IDPRODUCTO id_producto,
+       --p.NOMBREPRODUCTO producto,
+       col.IDCOLOR id_color,
+       col.DESCRIPCION color,
+       cat.IDCATEGORIA id_categoria,
+       cat.DESCRIPCION categoria,
+       SUM (d.CANTIDAD) cantidad_vendida,
+       COUNT ( distinct f.IDCLIENTE)
+from PRODUCTO p
+         left join COLOR col on (p.COLOR_IDCOLOR = col.IDCOLOR)
+         left join CATEGORIA cat on (p.CATEGORIA_IDCATEGORIA = cat.IDCATEGORIA)
+         left join DETALLEVENTA d on (p.IDPRODUCTO = d.IDPRODUCTO)
+        left join FACTURAVENTA f on (d.NUMEROORDEN = f.NUMEROORDEN)
+group by grouping sets (
+    (col.DESCRIPCION,col.IDCOLOR),
+    --((p.NOMBREPRODUCTO, p.IDPRODUCTO),(cat.IDCATEGORIA, cat.IDCATEGORIA)),
+    (p.IDPRODUCTO, CAT.IDCATEGORIA),
+    --((cat.IDCATEGORIA, cat.DESCRIPCION), (col.DESCRIPCION,col.IDCOLOR)),
+    (cat.IDCATEGORIA,col.IDCOLOR),
+    (cat.IDCATEGORIA, cat.DESCRIPCION)
+    );
+
+
+select p.IDPRODUCTO, p.COLOR_IDCOLOR, count (distinct f.IDCLIENTE) as cantidad_clientes_compraron
+from FACTURAVENTA f
+         join DETALLEVENTA d on d.NUMEROORDEN = f.NUMEROORDEN
+         right join PRODUCTO p on p.IDPRODUCTO = d.IDPRODUCTO
+group by p.IDPRODUCTO, p.COLOR_IDCOLOR;
